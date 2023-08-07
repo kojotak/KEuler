18a89bb234ca315864d77cfc2d67b00b7096ae82
Tomas Beneda <Tomas.Beneda@gmail.com>
2023-08-07 22:48:18 +0200

    minor kotlin simplifications

--- src/main/kotlin/cz/kojotak/keuler/Problem03.kt
+++ src/main/kotlin/cz/kojotak/keuler/Problem03.kt
@@ -5,10 +5,10 @@ import cz.kojotak.keuler.util.distinctPrimeFactors
 import kotlin.math.sqrt
 
 class Problem03 : EulerProblem {
-    override fun solve()= largestPrimeFactor(600851475143)
+    override fun solve() = largestPrimeFactor(600851475143)
 }
 
-fun largestPrimeFactor(number: Long) : Long = distinctPrimeFactors(number).last()
+fun largestPrimeFactor(number: Long) = distinctPrimeFactors(number).last()
 
-fun lsqrt(number : Long) : Long = sqrt(number.toDouble()).toLong()
-fun isqrt(number : Int) : Int = sqrt(number.toDouble()).toInt()
+fun lsqrt(number : Long) = sqrt(number.toDouble()).toLong()
+fun isqrt(number : Int) = sqrt(number.toDouble()).toInt()
