08be5e2e75edaac3bc0a6d8f6928773e261a2b00
Tomas Beneda <Tomas.Beneda@gmail.com>
2023-08-13 20:53:56 +0200

    problem 67 Maximum Path Sum II

--- src/main/kotlin/cz/kojotak/keuler/Problem67.kt
+++ src/main/kotlin/cz/kojotak/keuler/Problem67.kt
@@ -3,7 +3,7 @@ package cz.kojotak.keuler
 class Problem67 : Problem18() {
 
     override fun solve() = maximumPathSum(
-        this::class.java.classLoader.getResource("0067_triangle.txt")?.readText() ?: error(
+        this.javaClass.classLoader.getResource("0067_triangle.txt")?.readText() ?: error(
             "Can not read data for problem 67"
         )
     )
